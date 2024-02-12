123c17e5888983780c6a7c2e477acc89c3bd7fb5
ZhangTsingtao <rickzhangqt@gmail.com>
2024-02-11 22:25:02 -0500

    spheres

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ void processInput(GLFWwindow* window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_HEIGHT = 800;
 
 int main()
 {
@@ -110,6 +110,8 @@ int main()
         ourShader.use();
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         //glDrawArrays(GL_TRIANGLES, 0, 6);
+
+
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         // glBindVertexArray(0); // no need to unbind it every time 
 
--- test.vs
+++ test.vs
@@ -1,8 +1,13 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 out vec3 screenCoord;
+
+//uniform float screenZ;
 void main()
 {
     gl_Position = vec4(aPos, 1.0);
     screenCoord = (aPos + 1.0) / 2.0;
+
+    //set screen's z to negetive, thus FOV
+    screenCoord.z = -0.5;
 }
