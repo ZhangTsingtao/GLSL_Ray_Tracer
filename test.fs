66f62bea9e4e7462b0e9d5f7da733b3e0f3e95f3
ZhangTsingtao <rickzhangqt@gmail.com>
2024-02-22 21:56:56 -0500

    checkpoint2

--- test.fs
+++ test.fs
@@ -20,38 +20,23 @@ Ray RayConstructor(vec3 origin, vec3 direction)
 	return ray;
 }
 
-vec3 RayGetPointAt(Ray ray, float t)
-{
-	return ray.origin + t * ray.direction;
-}
-
 //sphere
 struct Sphere 
 {
     vec3 center;
     float radius;
+	vec3 color;
 }; 
 
-Sphere SphereConstructor(vec3 center, float radius)
+Sphere SphereConstructor(vec3 center, float radius, vec3 color)
 {
 	Sphere sphere;
 	sphere.center = center;
 	sphere.radius = radius;
+	sphere.color = color;
 	return sphere;
 }
 
-// bool SphereHit(Sphere sphere, Ray ray)
-// {
-// 	vec3 oc = ray.origin - sphere.center;
-	
-// 	float a = dot(ray.direction, ray.direction);
-// 	float b = 2.0 * dot(oc, ray.direction);
-// 	float c = dot(oc, oc) - sphere.radius * sphere.radius;
-
-// 	float discriminant = b * b - 4 * a * c;
-
-// 	return discriminant > 0.0;
-// }
 bool SphereHit(Sphere sphere, Ray ray, out float tnear)
 {
 	vec3 L = sphere.center - ray.origin;
@@ -67,11 +52,13 @@ bool SphereHit(Sphere sphere, Ray ray, out float tnear)
 struct Triangle
 {
 	vec3[3] points;
+	vec3 color;
 };
-Triangle TriangleConstructor(vec3[3] points)
+Triangle TriangleConstructor(vec3[3] points, vec3 color)
 {
 	Triangle triangle;
 	triangle.points = points;
+	triangle.color = color;
 	return triangle;
 }
 bool InTriangle(Triangle triangle, Ray ray, out float tnear)
@@ -100,35 +87,41 @@ void main()
 {
 	Ray ray = RayConstructor(camPos, screenCoord - camPos);
 
-	Sphere sphere0 = SphereConstructor(vec3(0.6, 0.6, -2), 0.7);
-	Sphere sphere1 = SphereConstructor(vec3(-0.3, -0.15, -2.6), 0.6);
-	Triangle triangle0 = TriangleConstructor(vec3[](vec3(-3, -1, -1.1), vec3(2, -1, -1.1), vec3(-3, -1, -8))); //left front, right front, left back
-	Triangle triangle1 = TriangleConstructor(vec3[](vec3(2, -1, -1.1), vec3(2, -1, -8), vec3(-3, -1, -8))); //right front, right back, left back
-	//test ray direction
-	// float dotCos = dot( ray.direction , vec3(0,0,-1));
-	// FragColor = vec4(dotCos,0, 0, 1.0);
+	Sphere sphere0 = SphereConstructor(vec3(0.6, 0.6, -2), 0.7, vec3(0.5, 0.1, 0.1));
+	Sphere sphere1 = SphereConstructor(vec3(-0.3, -0.15, -2.6), 0.6, vec3(0.3, 0.3, 0.1));
+	Triangle triangle0 = TriangleConstructor(vec3[](vec3(-3, -1, -1.1), vec3(2, -1, -1.1), vec3(-3, -1, -8)), vec3(0.1, 0.5, 0.1)); //left front, right front, left back
+	Triangle triangle1 = TriangleConstructor(vec3[](vec3(2, -1, -1.1), vec3(2, -1, -8), vec3(-3, -1, -8)), vec3(0.1, 0.5, 0.1)); //right front, right back, left back
 
+	Sphere spheres[2];
+	spheres[0] = sphere0;
+	spheres[1] = sphere1;
+	Triangle triangles[2];
+	triangles[0] = triangle0;
+	triangles[1] = triangle1;
+
+	
 	//background color
 	FragColor = vec4(0.1,0,0.2, 1.0);
 
-	//plane
-	float t;
-	if(InTriangle(triangle0, ray, t)){
-		FragColor = vec4(0, 0.5, 0, 1.0);
+	float distance = 999999;
+	for(int i = 0; i < triangles.length(); i++)
+	{
+		float t;
+		if (InTriangle(triangles[i], ray, t) && t < distance)
+		{
+			distance = t;
+			FragColor = vec4(triangles[i].color, 1.0);
+		}
 	}
-	if(InTriangle(triangle1, ray, t)){
-		FragColor = vec4(0, 0.5, 0, 1.0);
+	for(int i = 0; i < spheres.length(); i++)
+	{
+		float t;
+		if (SphereHit(spheres[i], ray, t) && t < distance)
+		{
+			distance = t;
+			FragColor = vec4(spheres[i].color, 1.0);
+		}
 	}
-	
-	if(SphereHit(sphere1,ray, t)){
-		FragColor = vec4(0.8,0, 0, 1.0);
-	}
-	if(SphereHit(sphere0,ray, t)){
-		FragColor = vec4(1,0, 0, 1.0);
-	}
-	
 
-	
-	
 
 }
